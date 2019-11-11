5aff35357df4bd75749796f858ca84c4e56aedd7
suraj7852 <surajpj7852@gmail.com>
2019-11-11 16:23:47 +0530

    compute c + a / b

--- useCase.sh
+++ useCase.sh
@@ -3,4 +3,4 @@ read -p "Enter first value:" firstValue
 read -p "Enter second value:" secondValue
 read -p "Enter third value:" thirdValue
 
-Total=$(( $firstValue * $secondValue + $thirdValue ))
+Total=$(( $thirdValue + $firstValue / $secondValue ))
