fe75573950539d6c6278077ec394fb391bd28f8e
suraj7852 <surajpj7852@gmail.com>
2019-11-11 17:14:32 +0530

    stored the result in the dictionary for every computation

--- useCase.sh
+++ useCase.sh
@@ -3,7 +3,13 @@ read -p "Enter first value:" firstValue
 read -p "Enter second value:" secondValue
 read -p "Enter third value:" thirdValue
 
-Total1=$(( $thirdValue + $firstValue * $secondValue ))
-Total2=$(( $firstValue * $secondValue + $thirdValue ))
-Total3=$(( $thirdValue + $firstValue / $secondValue ))
-Total4=$(( $firstValue % $secondValue / $thirdValue ))
+declare -A computes
+
+computes[Total1]=$(( $thirdValue + $firstValue * $secondValue ))
+computes[Total2]=$(( $firstValue * $secondValue + $thirdValue ))
+computes[Total3]=$(( $thirdValue + $firstValue / $secondValue ))
+computes[Total4]=$(( $firstValue % $secondValue / $thirdValue ))
+
+echo "key" ${!computes[@]}
+echo "value" ${computes[@]}
+
